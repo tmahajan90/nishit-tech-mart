f4305a70d6a72984c14f7338d13a1811b36194d1
Tarun <mahajan.tarun1990@gmail.com>
2021-09-25 22:22:11 +0530

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 User.delete_all
 User.create([{ name: 'Tarun', email: 'tarun@gmail.com', password: "password", remember_created_at: nil }])
+
+VehicleType.create([{ name: 'Flash' }, { name: 'Optima' }, { name: 'Photon' }, { name: 'Nyx' }, { name: 'Dash' }])
